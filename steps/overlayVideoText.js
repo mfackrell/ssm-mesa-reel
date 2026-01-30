5369310c2ba16c65efa7b22c249ada3f3a0e4a51
mfackrell <mfackrell@contract-cfo.com>
2026-01-30 13:36:11 -0600

    Update overlayVideoText.js

--- steps/overlayVideoText.js
+++ steps/overlayVideoText.js
@@ -31,7 +31,7 @@ export async function overlayVideoText(videoUrl, scriptLines) {
 
   const payload = {
     videoUrl: videoUrl,
-    audioUrl,
+    audioUrl: audioUrl,
     overlays: overlays
   };
 
